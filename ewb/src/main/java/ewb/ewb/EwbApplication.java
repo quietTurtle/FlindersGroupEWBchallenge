a5b3777c32a0f0e2493859145c08b0e8b3bfd14d
quietTurtle <54259846+quietTurtle@users.noreply.github.com>
2024-04-23 11:42:22 +0930

    half finished code with files
    honeslty mate i dont mind if you throw it into Chat gpt

---
+++ ewb/src/main/java/ewb/ewb/EwbApplication.java
@@ -0,0 +1,13 @@
+package ewb.ewb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EwbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EwbApplication.class, args);
+	}
+
+}
